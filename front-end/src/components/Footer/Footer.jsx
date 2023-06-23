a19b7ef454d28c1d64e9ae697797cbb8c8d45af5
Oyebolade Oladokun <oyeboladeoluwaferanmi@gmail.com>
2023-06-23 05:53:13 +0100

    Add new features

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React from "react";
 import "./Footer.css";
+import { facebookLogo, instagramLogo, twitterLogo } from "../../assets";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="black">
+      <div className="wrapper">
+        <div className="content-container">
+          <div className="links">
+            <a href="" className="logo">
+              Sound<span className="red">DZign</span>
+            </a>
+            <div className="social-icons">
+              <a href="#">
+                <img src={facebookLogo} alt="" />
+              </a>
+              <a href="#">
+                <img src={twitterLogo} alt="" />
+              </a>
+              <a href="#">
+                <img src={instagramLogo} alt="" />
+              </a>
+            </div>
+            <div className="copyright">
+              This website is design by GTCoding @ 2023
+            </div>
+          </div>
+          <div className="links">
+            <h3>Quick Links</h3>
+            <ul>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+            </ul>
+          </div>
+          <div className="links">
+            <h3>Quick Links</h3>
+            <ul>
+              <li>
+                <a href="#">oyeboladeoluwaferanmi@gmail.com</a>
+              </li>
+              <li>
+                <a href="#">+2347031170572</a>
+              </li>
+            </ul>
+          </div>
+          <div className="copyright mobile">
+              This website is design by GTCoding @ 2023
+            </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
